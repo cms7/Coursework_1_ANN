bc4bae61388c84e4ef6ce67e63c9a39942996adc
cms7 <cms7@hw.ac.uk>
2021-10-11 15:54:51 +0100

    Initial Start of Network

--- #coursework 1.py
+++ #coursework 1.py
@@ -2,10 +2,26 @@
 
 import pandas as pd
 import numpy as np
+from random import random
+from random import seed
 
 class NeuralNetwork:
 
+    #function to intitialise a network taking adjustable numbers of inputs, hidden and outputs 
+    def create_network(n_inputs,n_hidden,n_outputs):
+        network = list()
+        hidden_layer = [{'weights':[random() for i in range(n_inputs + 1)]} for i in range(n_hidden)]
+        output_layer = [{'weights':[random() for i in range(n_hidden + 1)]} for i in range(n_outputs)]
+        network.append(hidden_layer)
+        network.append(output_layer)
+        return network
 
+    #seed(1) ?? is this necessary 
+    network = create_network(2,1,2)
+    for layer in network:
+        print(layer)
+
+    
     #activation functions which takes an imput and produces a number between 0-1
     def sigmoid(inpt):
         return 1/1+np.exp(-inpt)
@@ -23,7 +39,7 @@ class NeuralNetwork:
     learning_rate = 0.1;
 
     data = pd.read_csv("DATASET.csv")
-    data.hist("Output")
+
 
         
 
