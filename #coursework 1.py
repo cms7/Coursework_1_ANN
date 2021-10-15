f5b2bb8127db4889b8dbdcb627d58f832f542696
cms7 <cms7@hw.ac.uk>
2021-10-15 09:07:01 +0100

    Added forward propagation, choice of active func

--- #coursework 1.py
+++ #coursework 1.py
@@ -1,46 +1,72 @@
-#coursework 1
+# coursework 1
 
 import pandas as pd
 import numpy as np
-from random import random
-from random import seed
+from random import random, seed
 
-class NeuralNetwork:
+# function to intitialise a network taking adjustable numbers of inputs, hidden and outputs
+# funtion will generate random weights for each input value, a bias is also genererated and will be the last element in the array
+# e.g. 3 inputs: weights = [0.1 , 0.3 , 0.2, 0.6] where element [-1] is the bias
+def initialize_network(n_inputs, n_hidden, n_outputs):
+    network = list()
+    w1 = [{'weights': [random() for i in range(n_inputs+1)]} for i in range(n_hidden)]
+    w2 = [{'weights': [random() for i in range(n_hidden+1)]} for i in range(n_outputs)]
+    network.append(w1)
+    network.append(w2)
+    return network
 
-    #function to intitialise a network taking adjustable numbers of inputs, hidden and outputs 
-    def create_network(n_inputs,n_hidden,n_outputs):
-        network = list()
-        hidden_layer = [{'weights':[random() for i in range(n_inputs + 1)]} for i in range(n_hidden)]
-        output_layer = [{'weights':[random() for i in range(n_hidden + 1)]} for i in range(n_outputs)]
-        network.append(hidden_layer)
-        network.append(output_layer)
-        return network
+# hyperparameters
+learning_rate = 0.1
+num_epochs = 200
+activation_func = "sigmoid"
 
-    #seed(1) ?? is this necessary 
-    network = create_network(2,1,2)
+# activation functions which takes an imput and produces a number between 0-1
+def sigmoid(inpt):
+    return 1/1+np.exp(-inpt)
+
+def tanh(inpt):
+    return np.tanh(inpt)
+
+def relu(inpt):
+    return max(inpt, 0)
+
+
+def sum_of_weights(weights, inputs):
+	sum = weights[-1]   #this is the value for the bias
+	for i in range(len(weights)-1):
+		sum += weights[i] * inputs[i]
+	return sum
+
+def forward_prop(network,input):
     for layer in network:
-        print(layer)
+        temp = []
+        for neuron in layer:
+            sum = sum_of_weights(neuron['weights'],input)
+            if(activation_func == "sigmoid"):
+                neuron['output'] = sigmoid(sum)
+            elif(activation_func == "relu"):
+                neuron['output'] = relu(sum)
+            elif(activation_func == "tanh"):
+                neuron['output'] = tanh(sum)
+            else:
+                print("Please check hyperparameter activation_function")
+            temp.append(neuron['output'])
+        input = temp
+    output = input
+    return output
 
-    
-    #activation functions which takes an imput and produces a number between 0-1
-    def sigmoid(inpt):
-        return 1/1+np.exp(-inpt)
-
-    def sigmoid_derivitive(inpt):
-        return sigmoid(inpt)*(1-sigmoid(inpt))
-
-    def tanh(inpt):
-        return np.tanh(inpt)
-
-    def relu(inpt):
-        return max(inpt,0)
-
-    #hyperparameters
-    learning_rate = 0.1;
-
-    #code for interpreting the data
-    data = pd.read_csv("DATASET.csv")
-    n_inputs = (len(list(data))-1)*(len(data))
-    n_outputs = 2   
+network = initialize_network(2,1,2)
+row = [1, 0, None]
+output = forward_prop(network, row)
+print(output)
 
 
+# calculates the derivative of the neuron output
+def neuron_derivitive(neuron_output):
+    return neuron_output*(1-neuron_output)
+
+
+
+# code for interpreting the data
+data = pd.read_csv("DATASET.csv")
+n_inputs = (len(list(data))-1)*(len(data))
